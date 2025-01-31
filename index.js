680fcc26145931c0587238c67caef870e9f051db
Lacey Merryman <merrymanlacey@gmail.com>
2025-01-30 15:04:35 -1000

    modify initial

---
+++ index.js
@@ -0,0 +1,42 @@
+// Your code here
+var questionsArr = [
+    {
+        question: 'Honolulu is located on the islandof Oahu.', 
+        answer: true
+    },
+    {
+        question: 'Pineapples grow on trees.',
+        answer: false
+    },
+    {
+        question: 'Rainbows are always in the same order of colors.',
+        answer: true
+    },
+    {
+        question: 'The word Aloha means hello and goodbye in the Hawaiian language.',
+        answer: true
+    },
+    {
+        question: 'Sharks are mammals.',
+        answer: false
+    }
+];
+
+// Function to run the quiz
+function runQuiz {
+    let correctAnswers = 0; //To keep track of correct answers
+
+    //Loop through the questions array
+    for (let i = 0; i < questionsArr.length; i++) {
+        //Ask the question using the confirm dialog, which returns truw or false
+        let userAnswer = confirm(questionsArr[i].question);
+
+        //Check if the user's answer matches the correct answer
+        if (userAnswer === questionsArr[i].answer) {
+            correctAnswers++;
+        }
+    }
+
+    //Calculate percentage of correct answers
+    let percentage = Math.round((correctAnswers / questionsArr.length) * 100);
+}
